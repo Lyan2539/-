b90678dd70a0d5c20750768018921bdea7108be1
Lyan2539 <45326262+Lyan2539@users.noreply.github.com>
2019-05-11 19:19:00 -0400

    start

---
+++ Solution-A1/DiaryWeb/Handler1.ashx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace DiaryWeb
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            // read the path of tamplet file
+            string filePath = context.Request.MapPath("Diary.html");
+            // read the file
+            string fileContent = File.ReadAllText(filePath);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Solution-A1/DiaryWeb/Handler2.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiaryWeb
+{
+    /// <summary>
+    /// Summary description for Handler2
+    /// </summary>
+    public class Handler2 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
